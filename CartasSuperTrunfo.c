1177266ef3e2a2451256a9a8bd834927c4e89a4a
DanielCavalli2777 <leaodrj@gmail.com>
2025-02-20 02:22:49 +0000

    alinhando os codigos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,7 +9,7 @@ int main(){
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
-    Estado (char[são Paulo])
+    Estado(char[são Paulo])
     Codigo(char[A1])
     Nome da cidade(char[São paulo])
     População(int) 12325000
